597c654ae9b27b30669724730fe83aa75543d1d2
Cole Wilkes <wilkescole@gmail.com>
2017-10-26 11:02:55 -0600

    Initial Commit

---
+++ CoreML-Workshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML-Workshop
+//
+//  Created by Cole Wilkes on 10/26/17.
+//  Copyright © 2017 viacole.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
